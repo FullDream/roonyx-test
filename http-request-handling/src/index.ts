98c5c20ef342fb057bc5b37c299808a97959b849
Pavel Rybakin <pvrybakin@gmail.com>
2022-10-06 22:51:20 +0300

    completed the first task

--- http-request-handling/src/index.ts
+++ http-request-handling/src/index.ts
@@ -1,12 +1,8 @@
-enum RickAndMortyUrls {
-	episodes = 'https://rickandmortyapi.com/api/episode',
-}
-
 interface Info {
 	count: number
 	pages: number
-	next: string
-	prev?: any
+	next?: string
+	prev?: string
 }
 
 interface Episode {
@@ -19,19 +15,77 @@ interface Episode {
 	created: Date
 }
 
-interface Episodes {
+interface EpisodesPage {
 	info: Info
 	results: Episode[]
 }
 
+interface NameAndUrl {
+	name: string
+	url: string
+}
+
+interface Character {
+	id: number
+	name: string
+	status: string
+	species: string
+	type: string
+	gender: string
+	origin: NameAndUrl
+	location: NameAndUrl
+	image: string
+	episode: string[]
+	url: string
+	created: Date
+}
+
+const episodesUrl = 'https://rickandmortyapi.com/api/episode'
+
 const getApi = async <T = unknown>(url: string): Promise<T> => {
-	const res = await fetch(url, { mode: 'cors' })
+	const res = await fetch(url)
 	return res.json()
 }
 
-let newEpisodes: any
+const getAllEpisodes = async () => {
+	const allEpisodes: Episode[] = []
 
-getApi<Episodes>(RickAndMortyUrls.episodes).then((data) => {
-	console.log(data.results)
-	document.querySelector('code')?.insertAdjacentText('afterend', JSON.stringify(data.results))
-})
+	const getPageRecursive = async (url: string) => {
+		const page = await getApi<EpisodesPage>(url)
+		allEpisodes.push(...page.results)
+
+		if (!page?.info?.next) return
+		await getPageRecursive(page.info.next)
+	}
+
+	await getPageRecursive(episodesUrl)
+
+	return allEpisodes
+}
+
+const getAllCharactersByUrls = async (urls: string[]) => {
+	const uniqueUrls = [...new Set(urls)]
+	const cache: { [key: string]: Character } = {}
+
+	for (const item of uniqueUrls) {
+		cache[item] = await getApi<Character>(item)
+	}
+
+	return cache
+}
+
+;(async () => {
+	const episodes = await getAllEpisodes()
+
+	const urls: string[] = []
+	episodes.forEach((episode) => urls.push(...episode.characters))
+
+	const characters = await getAllCharactersByUrls(urls)
+
+	const transformEpisodes = episodes.map((item) => ({
+		...item,
+		characters: item.characters.map((char) => characters[char]),
+	}))
+
+	console.log(transformEpisodes)
+})()
